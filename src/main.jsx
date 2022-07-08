0e1c16518f8ebf6f499a4b00ce4f9012c65e7fdd
thanadon-tkp <80769114+thanadon-tkp@users.noreply.github.com>
2022-07-08 13:20:30 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 )
